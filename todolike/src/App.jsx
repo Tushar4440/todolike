7914673620972d9cca8df8e29ef692456c0335b2
Tushar4440 <tushartewari33@gmail.com>
2025-04-26 20:54:07 +0530

    first commit

---
+++ todolike/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [items, setItems] = useState([{ text: "Tushar Tewari", checked: false }, { text: "Tushar Adhikari", checked: false }, { text: "Manish Joshi", checked: false }])
+
+  const removebtn = (index) => {
+    setItems(items.filter((_, i) => i !== index))
+  }
+
+  const togglebtn = (index) => {
+    setItems(items.map((item, i) =>
+      i === index ? { ...item, checked: (item.checked === false ? true : false) } : item
+    )
+    );
+  }
+
+  return (
+    <>
+      <h1 className="heading-top">Todo like aPP</h1><hr />
+      <div className='header'>
+        <ul>
+          {
+            items.map((item, index) => (
+              <li key={index}>
+                <input type="checkbox" checked={items.checked} onChange={() => togglebtn(index)} />
+                {item.text}{
+                  (item.checked === true) && (
+                    <button onClick={() => removebtn(index)}>x</button>
+                  )
+                }
+              </li>
+            ))
+          }
+        </ul>
+        <p>click on the link to access this page <a href="https://4hd2spn7-5173.inc1.devtunnels.ms/">https://4hd2spn7-5173.inc1.devtunnels.ms/</a>  </p>
+      </div >
+    </>
+  )
+}
+
+export default App
